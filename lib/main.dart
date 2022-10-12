d1c7b0c9cffd5721f57978d6740f8d416207623b
Luis Fernando M. Petreca <lfpetreca02@gmail.com>
2022-10-12 13:42:08 -0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter App'),
+      ),
+      body: Column(
+        children: const <Widget>[
+          Card(
+            child: Text('Chart PlaceHolder'),
+          ),
+          Card(
+            child: Text('List of Transations'),
+          )
+        ],
+      ),
+    );
+  }
+}
